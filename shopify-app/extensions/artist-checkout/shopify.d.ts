ee803a676a4c30e007930d7ecec9d970d64ba586
tylerdeterman12 <38754866-tylerdeterman12@users.noreply.replit.com>
2026-04-09 18:53:35 +0000

    Git commit prior to merge

--- shopify-app/extensions/artist-checkout/shopify.d.ts
+++ shopify-app/extensions/artist-checkout/shopify.d.ts
@@ -1,13 +1,9 @@
 import '@shopify/ui-extensions';
 
 //@ts-ignore
-declare module './src/Tile.jsx' {
-  const shopify: import('@shopify/ui-extensions/pos.home.tile.render').Api;
-  const globalThis: { shopify: typeof shopify };
-}
-
-//@ts-ignore
-declare module './src/Modal.jsx' {
-  const shopify: import('@shopify/ui-extensions/pos.home.modal.render').Api;
+declare module './src/index.tsx' {
+  const shopify:
+    | import('@shopify/ui-extensions/pos.home.tile.render').Api
+    | import('@shopify/ui-extensions/pos.home.modal.render').Api;
   const globalThis: { shopify: typeof shopify };
 }
